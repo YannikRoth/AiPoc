54f25d26d929e6b70840cfc70bc1de11c6247523
Yannik Roth [e523094] <yannik.roth@sbb.ch>
2025-10-20 21:54:40 +0200

    init project

---
+++ aipoc/src/main/java/ch/roth/aipoc/AipocApplication.java
@@ -0,0 +1,13 @@
+package ch.roth.aipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AipocApplication.class, args);
+    }
+
+}
---
+++ aipoc/src/test/java/ch/roth/aipoc/AipocApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.roth.aipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
